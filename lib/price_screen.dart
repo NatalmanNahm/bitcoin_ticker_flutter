b234ed4f7dc1d9c15d861deb9a31ed016e674b18
Natalman <natalnahm@gmail.com>
2020-04-13 19:54:59 -0500

    Added a for loop and a method that uses it to create a list of dropdownmenu item.

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -8,6 +9,21 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
 
   String seletectedCurrency = 'USD';
+  CoinData coinData = CoinData();
+
+  List<DropdownMenuItem> getDropdownItems(){
+    List<DropdownMenuItem<String>> dropDownItems = [];
+
+    for(String currency in currenciesList){
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropDownItems.add(newItem);
+    }
+
+    return dropDownItems;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,26 +63,13 @@ class _PriceScreenState extends State<PriceScreen> {
             color: Colors.lightBlue,
             child: DropdownButton<String>(
               value:seletectedCurrency,
-                items: [
-                  DropdownMenuItem(
-                    child: Text('USD'),
-                    value: 'USD',
-                  ),
-                  DropdownMenuItem(
-                    child: Text('EUR'),
-                    value: 'EUR',
-                  ),
-                  DropdownMenuItem(
-                    child: Text('GBP'),
-                    value: 'GBP',
-                  )
-                ],
-                onChanged: (value){
-                  setState(() {
-                    seletectedCurrency = value;
-                    print(seletectedCurrency);
-                  });
-                }
+              items: getDropdownItems(),
+              onChanged: (value){
+                setState(() {
+                  seletectedCurrency = value;
+                  print(seletectedCurrency);
+                });
+              }
             ),
           ),
         ],
