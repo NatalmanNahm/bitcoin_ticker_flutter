878a37958bf6710dd7a78d5022eb9b5add9857e9
Natalman <natalnahm@gmail.com>
2020-04-14 19:24:39 -0500

    Just added a check to see if we are using ios or oandroid device then use a picker or a dropdown menu.

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -32,8 +33,6 @@ class _PriceScreenState extends State<PriceScreen> {
         },
     );
   }
-
-  
 
   CupertinoPicker iOSPicker(){
     List<Text> pickerItemList = [];
@@ -87,7 +86,8 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: ),
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+          ),
         ],
       ),
     );
